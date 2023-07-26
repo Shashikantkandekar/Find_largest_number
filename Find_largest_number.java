769613aed61ed42be444172b8f5a9365a3936e94
Shashikant Sanjay Kandekar <126461951+Shashikantkandekar@users.noreply.github.com>
2023-07-26 19:07:56 +0530

    Add files via upload

---
+++ Find_largest_number.java
@@ -0,0 +1,18 @@
+public class Find_largest_number{
+    public static void main(String args [] )
+    {
+        int a =330;
+        int b =440;
+        int c = 111;
+
+        if(a>b && a>c){
+        System.out.println("Number a is greater....");
+    }
+        if(b>a && b>c){
+        System.out.println("Number b is greater....");
+    }
+        if(c>a && c>b) {
+        System.out.println("Number b is greater....");
+    }
+    }
+}
